812c65a5a721be7ee5fbf0af90794c2a2e15f47f
iqbalnur21 <m.iqbalnur21.min@gmail.com>
2022-11-20 18:04:49 +0800

    initial

---
+++ views_old/personal/index.php
@@ -0,0 +1,53 @@
+<?php
+
+use app\models\Personal;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/** @var yii\web\View $this */
+/** @var app\models\PersonalSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Personals';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="personal-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Personal', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'nama_lengkap',
+            'jenis_kelamin',
+            'tempat_lahir',
+            'tanggal_lahir',
+            //'status_perkawinan',
+            'agama',
+            //'pendidikan',
+            //'alamat',
+            //'no_ktp',
+            //'no_hp',
+            //'email:email',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Personal $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id_personal' => $model->id_personal]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
